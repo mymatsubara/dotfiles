be4e809cee2d4866e0f265a917f081a827e84612
Murilo Matsubara <murilo.matsubara@gmail.com>
2025-09-25 15:51:32 -0300

    create ~/.bashrc if it does not exists

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 LOAD_BASHRC_DM="source $DOTFILES_DIR/.bashrc"
 
+touch ~/.bashrc
+
 # Add source line to ~/.bashrc if it doesn't already exist
 if ! grep -Fxq "$LOAD_BASHRC_DM" ~/.bashrc; then
     echo "$LOAD_BASHRC_DM" >> ~/.bashrc
